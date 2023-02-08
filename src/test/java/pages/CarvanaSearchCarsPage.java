c729c5731c54cbc0a7b9d7cea78e7d9e7ccbb9ca
TechGlobalSchool <tgschool01@gmail.com>
2023-02-08 17:59:53 -0600

    part 2

--- src/test/java/pages/CarvanaSearchCarsPage.java
+++ src/test/java/pages/CarvanaSearchCarsPage.java
@@ -12,4 +12,37 @@ public class CarvanaSearchCarsPage extends CarvanaBasePage {
 
     @FindBy(css = "button[data-qa='drop-down-wrap']")
     public List<WebElement> filterButtonList;
+
+    @FindBy(css = "input[data-qa='search-bar-input']")
+    public WebElement searchInputBox;
+
+    @FindBy(css = "picture[data-qa='base-vehicle-image']")
+    public List<WebElement> tileImageList;
+
+    @FindBy(css = "svg[data-qa='favorite-icon']")
+    public List<WebElement> tileFavIcon;
+
+    @FindBy(css = "div[class='tk-frame middle-frame']")
+    public List<WebElement> tileBodyList;
+
+    @FindBy(css = "div[data-qa='base-inventory-type']")
+    public List<WebElement> tileInventoryType;
+
+    @FindBy(css = "div[class='year-make']")
+    public List<WebElement> tileYearMakeList;
+
+    @FindBy(css = "div[class='trim-mileage']")
+    public List<WebElement> tileTrimMileageList;
+
+    @FindBy(css = "div[data-qa='price']")
+    public List<WebElement> tilePriceList;
+
+    @FindBy(css = "div[data-qa='monthly-payment']")
+    public List<WebElement> tileMonthlyPmntList;
+
+    @FindBy(css = "div[class='down-payment']")
+    public List<WebElement> tileDownPmntList;
+
+    @FindBy(css = "div[data-qa='shipping-cost']")
+    public List<WebElement> tileShipList;
 }
--- src/test/java/scripts/CarvanaSearchCarsTest.java
+++ src/test/java/scripts/CarvanaSearchCarsTest.java
@@ -1,10 +1,15 @@
 package scripts;
 
+import org.apache.commons.lang3.RegExUtils;
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.CarvanaSearchCarsPage;
 import utilities.ElementHandler;
+import utilities.Waiter;
+
+import java.util.regex.Pattern;
 
 public class CarvanaSearchCarsTest extends CarvanaBase {
     @BeforeMethod
@@ -31,6 +36,70 @@ public class CarvanaSearchCarsTest extends CarvanaBase {
             Assert.assertTrue(carvanaSearchCarsPage.filterButtonList.get(i).isDisplayed());
             Assert.assertTrue(carvanaSearchCarsPage.filterButtonList.get(i).isEnabled());
             Assert.assertEquals(carvanaSearchCarsPage.filterButtonList.get(i).getText(), expectedData[i]);
+        }
+    }
+
+    /*
+        Test Case 6: Test name = Validate the search result tiles
+        Test priority = 6
+        Given user is on "https://www.carvana.com/"
+        When user clicks on "SEARCH CARS" link
+        Then user should be routed to "https://www.carvana.com/cars"
+        When user enters "mercedes-benz" to the search input box
+        And user clicks on "GO" button in the search input box
+        Then user should see "mercedes-benz" in the url
+        And validate each result tile
+        VALIDATION OF EACH TILE INCLUDES BELOW
+        Make sure each result tile is displayed with below information
+        1. an image
+        2. add to favorite button
+        3. tile body
+        ALSO VALIDATE EACH TILE BODY:
+        Make sure each tile body has below information
+        1. Inventory type - text should be displayed and should not be null or empty
+        2. Year-Make-Model information - text should be displayed and should not be null or empty
+        3. Trim-Mileage information - text should be displayed and should not be null or empty
+        4. Price - Make sure that each price is more than zero
+        5. Monthly Payment information - text should be displayed and should not be null or empty
+        6. Down Payment information - text should be displayed and should not be null or empty
+        7. Delivery chip must be displayed, and text is not null or empty
+     */
+    @Test(priority = 2, description = "Validate the search result tiles")
+    public void validateSearchResultTiles() {
+        carvanaSearchCarsPage.searchInputBox.sendKeys("mercedes-benz" + Keys.ENTER);
+        Waiter.pause(3);
+
+        for(int i = 0; i < carvanaSearchCarsPage.tileImageList.size(); i++) {
+            Assert.assertTrue(carvanaSearchCarsPage.tileImageList.get(i).isDisplayed());
+            Assert.assertTrue(carvanaSearchCarsPage.tileFavIcon.get(i).isDisplayed());
+            Assert.assertTrue(carvanaSearchCarsPage.tileBodyList.get(i).isDisplayed());
+
+            Assert.assertTrue(carvanaSearchCarsPage.tileInventoryType.get(i).isDisplayed());
+            Assert.assertNotNull(carvanaSearchCarsPage.tileInventoryType.get(i).getText());
+            Assert.assertFalse(carvanaSearchCarsPage.tileInventoryType.get(i).getText().equals(""));
+
+            Assert.assertTrue(carvanaSearchCarsPage.tileYearMakeList.get(i).isDisplayed());
+            Assert.assertNotNull(carvanaSearchCarsPage.tileYearMakeList.get(i).getText());
+            Assert.assertFalse(carvanaSearchCarsPage.tileYearMakeList.get(i).getText().equals(""));
+
+            Assert.assertTrue(carvanaSearchCarsPage.tileTrimMileageList.get(i).isDisplayed());
+            Assert.assertNotNull(carvanaSearchCarsPage.tileTrimMileageList.get(i).getText());
+            Assert.assertFalse(carvanaSearchCarsPage.tileTrimMileageList.get(i).getText().equals(""));
+
+            Assert.assertTrue(carvanaSearchCarsPage.tilePriceList.get(i).isDisplayed());
+            Assert.assertTrue(Integer.parseInt(RegExUtils.removeAll(carvanaSearchCarsPage.tilePriceList.get(i).getText(), Pattern.compile("\\D"))) > 0);
+
+            Assert.assertTrue(carvanaSearchCarsPage.tileMonthlyPmntList.get(i).isDisplayed());
+            Assert.assertNotNull(RegExUtils.removeAll(carvanaSearchCarsPage.tilePriceList.get(i).getText(), Pattern.compile("\\D")));
+            Assert.assertFalse(RegExUtils.removeAll(carvanaSearchCarsPage.tilePriceList.get(i).getText(), Pattern.compile("\\D")).equals(""));
+
+            Assert.assertTrue(carvanaSearchCarsPage.tileDownPmntList.get(i).isDisplayed());
+            Assert.assertNotNull(carvanaSearchCarsPage.tileDownPmntList.get(i).getText());
+            Assert.assertFalse(carvanaSearchCarsPage.tileDownPmntList.get(i).getText().equals(""));
+
+            Assert.assertTrue(carvanaSearchCarsPage.tileShipList.get(i).isDisplayed());
+            Assert.assertNotNull(carvanaSearchCarsPage.tileShipList.get(i).getText());
+            Assert.assertFalse(carvanaSearchCarsPage.tileShipList.get(i).getText().equals(""));
         }
     }
 }
